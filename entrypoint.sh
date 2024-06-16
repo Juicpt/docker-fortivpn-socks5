ab9b3669a2319bc5dd24ec2e1b46b6767b47f2db
Juicpt <Juicpt@live.com>
2024-06-16 23:18:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 /usr/bin/glider -listen :8443 &
-echo "http/socks5 proxy server: $(hostname -i):8443"
+echo "http/socks5 proxy server: $(hostname -i):8443" &
+mkdir -p /run/ppp/
 exec "$@"
